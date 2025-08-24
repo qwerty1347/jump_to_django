639cd14e75b348873f7e953f363417b18395b5ed
DESKTOP\LEE <test>
2025-08-24 18:26:07 +0900

    fix: question view 오류 처리 수정

--- apps/pybo/question/views.py
+++ apps/pybo/question/views.py
@@ -1,7 +1,5 @@
-from django.conf import settings
-from django.http import Http404, HttpResponse, JsonResponse
+from django.http import HttpResponse
 from django.shortcuts import render
-import environ
 
 from apps.pybo.question.services.service import QuestionService
 
@@ -11,21 +9,14 @@ question_service = QuestionService()
 
 def index(request):
     try:
-        result = question_service.get_questions()
-        # return HttpResponse(result)
-
-        # raise Http404("해당 질문을 찾을 수 없습니다.")
-
-        
-
-        print(settings.DEBUG, '=============@')
-
         return HttpResponse("Hello, Pybo Question!")
 
-        # raise Exception("테스트용 500 에러")
 
-        
+        result = question_service.get_questions()
+        return HttpResponse(f"<pre>{pformat(result)}</pre>")
+
+
 
     except Exception as e:
-        raise e
-    
+        print(f"ERROR: {str(e)}")
+        return render(request, "errors/500.html", status=500)
