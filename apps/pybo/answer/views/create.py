08aa24ae39349f987a06f46ffdd7273afa5d5da5
이건희 <qwertys1347@gmail.com>
2025-08-26 18:11:20 +0900

    2-06 데이터 저장 (question form 검증 실패 처리)

--- apps/pybo/answer/views/create.py
+++ apps/pybo/answer/views/create.py
@@ -1,9 +1,11 @@
 from django.contrib import messages
-from django.http import Http404, HttpResponse
-from django.shortcuts import redirect
+from django.http import Http404
+from django.shortcuts import redirect, render
 
 from apps.pybo.answer.forms.create import AnswerCreateForm
 from apps.pybo.answer.services.service import AnswerService
+from apps.pybo.question.repositories.repository import QuestionRepository
+from common.constants.template import TemplateConstants
 from common.utils.exception import handle_exception
 
 
@@ -14,16 +16,18 @@ def create_answer(request, question_id: int):
     # TODO: POST 외 405 처리
 
     form = AnswerCreateForm(request.POST)
+    question_repository = QuestionRepository()
 
     if not form.is_valid():
-        return HttpResponse("hello, form", status=422)
+        context = {'form': form, 'question': question_repository.get_question(question_id)}
+        return render(request, TemplateConstants.PYBO['question']['detail'], context)
 
     try:
         answer =  answer_service.create_answer(question_id, form.cleaned_data)
 
         if answer:
             messages.success(request, "답변 등록완료")
-            return redirect('pybo:question:detail', question_id)
+            return redirect('pybo:question:detail', question_id=question_id)
 
     except Http404:
         raise
