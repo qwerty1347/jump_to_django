6f6f57cd3ca991792e7a9059a29d99e60b438aa7
이건희 <qwertys1347@gmail.com>
2025-08-29 16:57:54 +0900

    2-10 폼 (question 모델폼 추가)

---
+++ apps/pybo/question/forms/create.py
@@ -0,0 +1,18 @@
+from django import forms
+
+from apps.pybo.question.models.question import Question
+
+
+class QuestionCreateForm(forms.ModelForm):
+
+    class Meta:
+        model = Question
+        fields = ['subject', 'content']
+        widgets = {
+            'subject': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 10}),
+        }
+        labels = {
+            'subject': '제목',
+            'content': '내용',
+        }
