577ea2573cf7be4ad9fe61366655d37e4d258176
이건희 <qwertys1347@gmail.com>
2025-09-11 18:11:34 +0900

    3-05 로그인과 로그아웃 (로그인 뷰 로그인 설정 추가)

--- apps/pybo/urls.py
+++ apps/pybo/urls.py
@@ -6,7 +6,7 @@ from . import views
 app_name= "pybo"
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name="index"),
     path('login/', auth_views.LoginView.as_view(template_name='pybo/common/login.html'), name="login"),
     path('question/', include('apps.pybo.question.urls', namespace="question")),
     path('answer/', include('apps.pybo.answer.urls', namespace="answer")),
--- config/settings.py
+++ config/settings.py
@@ -163,3 +163,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# 로그인 뷰, 로그인 성공 시 이동하는 URL
+LOGIN_REDIRECT_URL = 'pybo:index'
