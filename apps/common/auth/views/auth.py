5fcb17ab15f3e9af65c315702f7eb7c287b304af
이건희 <qwertys1347@gmail.com>
2025-09-16 11:55:11 +0900

    3-06 회원가입 (폼 오류, view 추가)

--- apps/common/auth/views/auth.py
+++ apps/common/auth/views/auth.py
@@ -1,5 +1,26 @@
 from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+
+from apps.common.auth.forms.user_create import UserCreateForm
+from common.constants.template import TemplateConstants
 
 
+@require_http_methods(["GET", "POST"])
 def signup(request: HttpRequest):
-    return HttpResponse("signup")
+    if request.method == "GET":
+        form = UserCreateForm()
+        return render(request, TemplateConstants.PYBO['user']['signup'], {'form': form})
+
+    elif request.method == "POST":
+        form = UserCreateForm(request.POST)
+
+        if form.is_valid():
+            try:
+                pass
+
+            except Exception as e:
+                raise e
+
+        else:
+            return render(request, TemplateConstants.PYBO['user']['signup'], {'form': form})
